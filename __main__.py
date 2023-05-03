90d6e27ae5ff5af7f2b57d893c2cbb0f986dad4b
David <dadch1404@gmail.com>
2023-05-03 11:05:35 +0000

    fix lint

--- __main__.py
+++ __main__.py
@@ -1,17 +1,18 @@
+"""
+Flask API to extract media URLs from various platforms, such as Twitter and Instagram.
+"""
+
 from flask import Flask, request, jsonify
 from platforms.twitter import get_media_url as get_twitter_media_url
-from platforms.instagram import get_media_url as get_instagram_media_url
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-INSTAGRAM_EMAIL = os.getenv('INSTAGRAM_EMAIL')
-INSTAGRAM_PASSWORD = os.getenv('INSTAGRAM_PASSWORD')
+from platforms.instagram import instagram_client
 
 app = Flask(__name__)
 
 @app.route('/api/get_media_url', methods=['POST'])
 def api_get_media_url():
+    """
+    Extract media URLs from a given URL.
+    """
     data = request.json
     url = data.get('url')
 
@@ -24,26 +25,34 @@ def api_get_media_url():
 
     try:
         media_urls = extract_media_url(url, platform)
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
+    except ValueError as exc:
+        return jsonify({'error': str(exc)}), 500
 
     return jsonify({'media_urls': media_urls})
 
 def get_platform(url):
+    """
+    Return the platform of a given URL.
+    """
     if 'twitter.com' in url:
         return 'twitter'
     elif 'instagram.com' in url:
         return 'instagram'
+    # Add more platforms here as they are implemented
     else:
         return None
 
 def extract_media_url(url, platform):
+    """
+    Extract media URLs from a given URL.
+    """
     if platform == 'twitter':
         return get_twitter_media_url(url)
     elif platform == 'instagram':
-        return get_instagram_media_url(url, INSTAGRAM_EMAIL, INSTAGRAM_PASSWORD)
+        return instagram_client.get_media_url(url)
+    # Add more platforms here as they are implemented
     else:
-        raise Exception('Unsupported platform')
+        raise ValueError('Unsupported platform')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- platforms/instagram.py
+++ platforms/instagram.py
@@ -1,9 +1,29 @@
+"""
+Module to handle Instagram media extraction using the instagrapi package.
+"""
+
 import os
 from instagrapi import Client
+from dotenv import load_dotenv
 
-def get_media_url(url, email, password):
-    client = Client()
-    client.login(email, password)
-    media_id = client.media_pk_from_url(url)
-    media = client.media_info(media_id)
-    return media.thumbnail_url, media.video_url
+load_dotenv(dotenv_path='../.env')
+
+class InstagramClient:
+    """
+    Class to handle Instagram media extraction using the instagrapi package.
+    """
+    def __init__(self, email, password):
+        self.client = Client()
+        self.client.login(email, password)
+
+    def get_media_url(self, url):
+        """
+        Return the media URL from a given Instagram URL.
+        """
+        media_id = self.client.media_pk_from_url(url)
+        media = self.client.media_info(media_id)
+        return media.thumbnail_url, media.video_url
+
+INSTAGRAM_EMAIL = os.environ.get("INSTAGRAM_EMAIL")
+INSTAGRAM_PASSWORD = os.environ.get("INSTAGRAM_PASSWORD")
+instagram_client = InstagramClient(INSTAGRAM_EMAIL, INSTAGRAM_PASSWORD)
